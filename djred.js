7e35ab7cd46eafb405012b6c726de1cf0c960942
starbuckshiker <u1304954@umail.utah.edu>
2020-11-19 19:37:15 -0700

    colors!

--- djred.js
+++ djred.js
@@ -1,26 +1,31 @@
 
     var sqaure = document.getElementById("square");
     var orginalColor = window.getComputedStyle(sqaure).backgroundColor;
-    console.log("this");
+    console.log("the");
 
     function hoverColor(color) {
-        sqaure.style.backgroundColor = color;
-    }
-    function leftClickColor(color) {
-        sqaure.style.backgroundColor = color;
-    }
-    function defaultColor(color) {
-        sqaure.style.backgroundColor = originalColor;
-    }
-    function scrollColor(color) {
-        sqaure.style.backgroundColor = color;
-    }
-    function upClickColor(color) {
-        sqaure.style.backgroundColor = color;
-    }
-    function doubleColor(color) {
         sqaure.style.backgroundColor = color;
     }
     function defaultColor(color) {
         sqaure.style.backgroundColor = orginalColor;
     }
+    console.log("mouse")
+
+    function leftClickColor(color) {
+        sqaure.style.backgroundColor = color;
+    }
+    function rightColor(color) {
+        sqaure.style.backgroundColor = color;
+    }
+    console.log("is")
+
+    function doubleColor(color) {
+        sqaure.style.backgroundColor = color;
+    }
+    function upClickColor(color) {
+        sqaure.style.backgroundColor = color;
+    }
+    function defaultColor(color) {
+        sqaure.style.backgroundColor = orginalColor;
+    }
+    console.log("HAPPY")
